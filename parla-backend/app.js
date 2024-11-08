2df81aee20f8765a8726b0662c40a16c0fb577cc
Engineernoob <t.denmark@outlook.com>
2024-11-07 20:47:35 -0600

    adding cors to files

--- parla-backend/app.js
+++ parla-backend/app.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(bodyParser.json());
 
